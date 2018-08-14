1a35fd7f6bad60906c2f7f1eacd4b336abb6b71a
Yurok86RUS <yurok.kolesnikov@gmail.com>
2018-08-14 23:43:17 +0500

    finish task 3

--- src/ru/geekbrains/lesson_2/Main.java
+++ src/ru/geekbrains/lesson_2/Main.java
@@ -25,5 +25,17 @@ public class Main {
                 j+=3;
                 System.out.print(arr2[i] + " ");
             }
+
+        //Task 3
+        System.out.println(" ");
+        System.out.println("Задание 3:");
+        int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i<arr3.length; i++) {
+            if (arr3[i] < 6 ) {
+                arr3[i] = arr3[i] * 2;
+            }
+            System.out.print(arr3[i] + " ");
+        }
+
     }
 }
